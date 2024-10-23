26271397cff31f263e4cdf4625a54cfc8367a22f
itsaryanchauhan1 <itsaryanchauhan1@gmail.com>
2024-10-23 21:11:13 +0530

    incominggg

---
+++ scripts/role-selection.js
@@ -0,0 +1,7 @@
+document.getElementById("evDriverButton").onclick = function() {
+    window.location.href = "ev-driver-form.html"; // Redirect to EV Driver form
+};
+
+document.getElementById("chargeProviderButton").onclick = function() {
+    window.location.href = "charging-provider-form.html"; // Redirect to Charging Provider form
+};
